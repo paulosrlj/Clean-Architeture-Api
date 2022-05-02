5aa82d6a0f49f74f0685d8e2a9ca873a18b067cc
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-05-02 13:50:25 -0300

    Revert "feat: ensure EmailValidatorAdapter returns true if validator returns true"
    This reverts commit 103a3817ec6cb586d65c7f9e96aad4ae9e9dd8df.

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,8 +1,7 @@
 import { EmailValidator } from '../presentation/protocols/email-validador'
 
-import validator from 'validator'
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    return validator.isEmail(email)
+    return false
   }
 }
