afb246873bf4f50dd94e562ceb04845e4f1ed6fa
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-05-13 12:29:07 -0300

    feat: add ExpressRouteAdapter

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://admin:admin@localhost:27017',
   port: process.env.PORT || 5050
 }
